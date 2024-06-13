fcd394a898159fd777c1214891ac7f70e23bfb86
Corbyn Ledbetter <corbynledbetter@my.unt.edu>
2024-06-13 10:20:38 -0500

    Made coding changes to mistakes

---
+++ LeetcodeProblems/TopKElement.py
@@ -0,0 +1,4 @@
+#Given an ineger array nums and an ineger k, return the total number of continuous subarrays whose sum equals to k.
+#Example 1:
+    #Input: nums = [1,2,2,3,3,3], k = 2
+    #Output: [2,3]
