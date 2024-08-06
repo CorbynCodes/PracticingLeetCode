5ddec76b0c3335a655eb11acb0d391e283fe8a51
Corbyn Ledbetter <corbynledbetter@my.unt.edu>
2024-08-06 14:29:57 -0500

    Did some leetcode today

--- LeetcodeProblems/TopKElement.py
+++ LeetcodeProblems/TopKElement.py
@@ -2,3 +2,20 @@
 #Example 1:
     #Input: nums = [1,2,2,3,3,3], k = 2
     #Output: [2,3]
+
+class soultion:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {} # is a dictionary that will store the frequency of each element
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) 1, 0, -1)
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
