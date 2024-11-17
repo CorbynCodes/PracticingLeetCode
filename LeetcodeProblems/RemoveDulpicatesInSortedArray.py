d6cff038a98be5181b67c7d6172a00672d4d2e7a
Corbyn Ledbetter <corbynledbetter@Corbyns-MacBook-Pro.local>
2024-11-17 17:29:57 -0600

    removingDulpicatesInSortedArray problem finshed

---
+++ LeetcodeProblems/RemoveDulpicatesInSortedArray.py
@@ -0,0 +1,8 @@
+class Soultion:
+    def removeDulpicates(self, nums: List[int]) -> int:
+        L = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[L] = nums[r]
+                L += 1
+        return L
